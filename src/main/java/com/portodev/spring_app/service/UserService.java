1b52aebbba9565001f2aeb0d78b6a0e2fcdc488f
Fernando Porto <fernandogomes143@gmail.com>
2024-10-09 23:11:57 -0300

    Ajuste UserService adicionado ResponseStatusException para retorno 404

--- src/main/java/com/portodev/spring_app/service/UserService.java
+++ src/main/java/com/portodev/spring_app/service/UserService.java
@@ -4,6 +4,8 @@ import com.portodev.spring_app.model.User;
 import com.portodev.spring_app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +24,12 @@ public class UserService {
     }
 
     public Optional<User> findById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+
+        if (!user.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado");
+        }
+
         return userRepository.findById(id);
     }
 
